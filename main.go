cca31c9215c08917925c71a215028ab85bad58de
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-06-08 08:42:08 -0400

    Refactor primitive call into func

--- main.go
+++ main.go
@@ -7,10 +7,30 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("primitive", strings.Fields("-i tmp/starry_night.jpg -o out.png -n 100")...)
-	b, err := cmd.CombinedOutput()
+	out, err := primitive("tmp/starry_night.jpg", "out.png", 100, triangle)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(b))
+	fmt.Println(out)
+}
+
+type PrimitiveMode int
+
+const (
+	combo PrimitiveMode = iota
+	triangle
+	rect
+	ellipse
+	circle
+	rotatedrect
+	beziers
+	rotatedellipse
+	polygon
+)
+
+func primitive(inputFile, outputFile string, numShapes int, mode PrimitiveMode) (string, error) {
+	argStr := fmt.Sprintf("-i %s -o %s -n %d -m %d", inputFile, outputFile, numShapes, mode)
+	cmd := exec.Command("primitive", strings.Fields(argStr)...)
+	b, err := cmd.CombinedOutput()
+	return string(b), err
 }
