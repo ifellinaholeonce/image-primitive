cb9c9403c2c94bfff70a98d332f9e3fe52aca4a0
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-06-08 21:36:53 -0400

    Upload image form

--- main.go
+++ main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"image-primitive/primitive"
 	"io"
-	"os"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	image, err := os.Open("tmp/starry_night.jpg")
-	if err != nil {
-		panic(err)
-	}
-	defer image.Close()
-	out, err := primitive.Transform(image, 50)
-	if err != nil {
-		panic(err)
-	}
-	os.Remove("out.jpg")
-	outFile, err := os.Create("out.jpg")
-	if err != nil {
-		panic(err)
-	}
-	io.Copy(outFile, out)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `<html></body>
+		<form action="/upload" method="post" enctype="multipart/form-data">
+			<input name="image" type="file">
+			<button type="submit">Upload Image</button>
+		</form>
+		</body></html>`
+		fmt.Fprint(w, html)
+	})
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		file, header, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer file.Close()
+		// TODO: Handle file extensions
+		ext := filepath.Ext(header.Filename)
+		_ = ext
+		out, err := primitive.Transform(file, 50)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "image/png")
+		io.Copy(w, out)
+	})
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
