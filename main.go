aa003b3e4b96094e622dee4b998e76c9a789ccd3
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-06-15 00:29:25 -0400

    Refactor generating options

--- main.go
+++ main.go
@@ -20,9 +20,11 @@ import (
 )
 
 type genOpts struct {
-	N        int
-	M        primitive.Mode
-	FilePath string
+	N           int
+	M           primitive.Mode
+	FilePath    string
+	Name        string
+	Fingerprint string
 }
 
 func main() {
@@ -94,52 +96,48 @@ func main() {
 		}
 		defer f.Close()
 		ext := filepath.Ext(f.Name())[1:]
+		var opts []genOpts
 		modeStr := r.FormValue("mode")
 		if modeStr == "" {
-			opts := []genOpts{
-				{N: 50, M: primitive.ModeBeziers},
-				{N: 50, M: primitive.ModeCombo},
-				{N: 50, M: primitive.ModeRotatedRect},
-				{N: 50, M: primitive.ModeRotatedEllipse},
+			DEFAULT_NUM := 10
+			opts = []genOpts{
+				{N: DEFAULT_NUM, M: primitive.ModeBeziers},
+				{N: DEFAULT_NUM, M: primitive.ModeCombo},
+				{N: DEFAULT_NUM, M: primitive.ModeRotatedRect},
+				{N: DEFAULT_NUM, M: primitive.ModeRotatedEllipse},
 			}
-			for i := range opts {
-				file, err := tempfile(ext)
-				if err != nil {
-					panic(err)
-				}
-				opts[i].FilePath = file.Name()
+		}
+		nStr := r.FormValue("n")
+		if nStr == "" && modeStr != "" {
+			mode, err := strconv.Atoi(modeStr)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
 			}
-			go renderModeChoices(w, r, f.Name(), ext, opts...)
-			html := buildHTML()
-			tpl := template.Must(template.New("").Parse(html))
-			var fingerprints []string
-			for _, opt := range opts {
-				base := filepath.Base(opt.FilePath)
-				fingerprints = append(fingerprints, base[0:len(base)-(len(ext)+1)])
+			opts = []genOpts{
+				{N: 10, M: primitive.Mode(mode)},
+				{N: 20, M: primitive.Mode(mode)},
+				{N: 40, M: primitive.Mode(mode)},
+				{N: 80, M: primitive.Mode(mode)},
 			}
-			err = tpl.Execute(w, fingerprints)
+		}
+		for i := range opts {
+			file, err := tempfile(ext)
 			if err != nil {
 				panic(err)
 			}
-			return
+			base := filepath.Base(file.Name())
+			opts[i].FilePath = file.Name()
+			opts[i].Name = base
+			opts[i].Fingerprint = base[0 : len(base)-(len(ext)+1)]
 		}
-		mode, err := strconv.Atoi(modeStr)
+		go renderModeChoices(w, r, f.Name(), ext, opts...)
+		tpl := buildTemplate()
+		err = tpl.Execute(w, opts)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+			panic(err)
 		}
-		nStr := r.FormValue("n")
-		if nStr == "" {
-			renderNumShapesChoices(w, r, f, ext, primitive.Mode(mode))
-			return
-		}
-		numShapes, err := strconv.Atoi(nStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		_ = numShapes
-		http.Redirect(w, r, "/img/"+filepath.Base(f.Name()), http.StatusFound)
+		// http.Redirect(w, r, "/img/"+filepath.Base(f.Name()), http.StatusFound)
 	})
 
 	fileServer := http.FileServer(http.Dir("./img/"))
@@ -157,45 +155,6 @@ func tempfile(ext string) (*os.File, error) {
 	return os.Create(fmt.Sprintf("%s.%s", in.Name(), ext))
 }
 
-func renderNumShapesChoices(w http.ResponseWriter, r *http.Request, f io.ReadSeeker, ext string, mode primitive.Mode) {
-	opts := []genOpts{
-		{N: 10, M: mode},
-		// {N: 20, M: mode},
-		// {N: 40, M: mode},
-		// {N: 80, M: mode},
-	}
-	imgs, err := genImages(f, ext, opts...)
-	if err != nil {
-		panic(err)
-	}
-
-	html := `<html><body>
-		{{range .}}
-			<a href="/transform/{{.Name}}?mode={{.Mode}}&n={{.NumShapes}}">
-				<img style="width: 20%;" src="/img/{{.Name}}">
-			</a>
-		{{end}}
-		</body></html>`
-	type dataStruct struct {
-		Name      string
-		Mode      primitive.Mode
-		NumShapes int
-	}
-	var data []dataStruct
-	for i, img := range imgs {
-		data = append(data, dataStruct{
-			Name:      filepath.Base(img),
-			Mode:      opts[i].M,
-			NumShapes: opts[i].N,
-		})
-	}
-	tpl := template.Must(template.New("").Parse(html))
-	err = tpl.Execute(w, data)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func renderModeChoices(w http.ResponseWriter, r *http.Request, imgPath string, ext string, opts ...genOpts) {
 	f, err := os.Open(imgPath)
 	if err != nil {
@@ -208,30 +167,6 @@ func renderModeChoices(w http.ResponseWriter, r *http.Request, imgPath string, e
 		panic(err)
 	}
 	_ = imgs
-	// TODO: Return this HTML async
-	// html := `<html><body>
-	// 	{{range .}}
-	// 		<a href="/transform/{{.Name}}?mode={{.Mode}}">
-	// 			<img style="width: 20%;" src="/img/{{.Name}}">
-	// 		</a>
-	// 	{{end}}
-	// 	</body></html>`
-	// type dataStruct struct {
-	// 	Name string
-	// 	Mode primitive.Mode
-	// }
-	// var data []dataStruct
-	// for i, img := range imgs {
-	// 	data = append(data, dataStruct{
-	// 		Name: filepath.Base(img),
-	// 		Mode: opts[i].M,
-	// 	})
-	// }
-	// tpl := template.Must(template.New("").Parse(html))
-	// err = tpl.Execute(w, data)
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func genImages(rs io.ReadSeeker, ext string, opts ...genOpts) ([]string, error) {
@@ -250,11 +185,13 @@ func genImages(rs io.ReadSeeker, ext string, opts ...genOpts) ([]string, error) 
 func genImage(r io.Reader, ext string, numShapes int, mode primitive.Mode, outFilePath string) (string, error) {
 	out, err := primitive.Transform(r, ext, numShapes, primitive.WithMode(mode))
 	if err != nil {
+		fmt.Println("failed to run primitive")
 		return "", err
 	}
 	dir, fName := filepath.Split(outFilePath)
 	outFile, err := os.Create(dir + "out_" + fName)
 	if err != nil {
+		fmt.Println("failed to create file")
 		return "", err
 	}
 	defer outFile.Close()
@@ -293,7 +230,7 @@ func writeToS3(file *os.File) {
 	fmt.Println("Uploaded", item)
 }
 
-func buildHTML() string {
+func buildTemplate() *template.Template {
 	html := `<html><body>
 			<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 			<script type="text/javascript">
@@ -319,30 +256,32 @@ func buildHTML() string {
 						.catch(onError);
 				}
 			</script>
-			<p>The names are going to be:</p>
+			<p>This can take a while</p>
 			<ul>
 				{{range.}}
-					<li id="{{.}}"><i class="fa fa-spinner fa-spin" style="font-size:24px"></i></li>
+					<li id="{{.Fingerprint}}"><i class="fa fa-spinner fa-spin" style="font-size:24px"></i></li>
 				{{end}}
 			</ul>
 			<script type="text/javascript">
 				async function getImg() {
 					await sleep(10000)
 					{{range.}}
-						console.log("loaded script for {{.}}")
-						console.log("fetching {{.}}")
-						await fetchRetry("http://localhost:3000/img/out_{{.}}.jpg", 2000, 10).then((val) => {
-							el = document.getElementById("{{.}}")
+						await fetchRetry("http://localhost:3000/img/out_{{.Fingerprint}}.jpg", 2000, 10).then((val) => {
+							el = document.getElementById("{{.Fingerprint}}")
+							link = document.createElement('a')
+							link.href = "/transform/{{.Name}}?mode={{.M}}"
 							var img = document.createElement('img')
-							img.src = "http://localhost:3000/img/out_{{.}}.jpg"
+							img.src = "http://localhost:3000/img/out_{{.Fingerprint}}.jpg"
 							img.style.width = "20%;"
-							img.id = "{{.}}"
-							el.parentNode.replaceChild(img, el)
+							img.id = "{{.Fingerprint}}"
+							el.parentNode.replaceChild(link, el)
+							link.appendChild(img)
 						})
 					{{end}}
 				}
 				getImg()
 			</script>
 			</body></html>`
-	return html
+	tpl := template.Must(template.New("").Parse(html))
+	return tpl
 }
