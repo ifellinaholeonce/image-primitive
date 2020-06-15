ad65d48f6a16efccedb7619ee9e05def84cb9ca0
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-06-14 23:00:58 -0400

    Move html

--- main.go
+++ main.go
@@ -110,56 +110,7 @@ func main() {
 				opts[i].FilePath = file.Name()
 			}
 			go renderModeChoices(w, r, f.Name(), ext, opts...)
-			html := `<html><body>
-			<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-			<script type="text/javascript">
-				async function sleep(ms) {
-					return new Promise(resolve => setTimeout(resolve, ms));
-				}
-			</script>
-			<script>
-				function fetchRetry(url, delay, tries) {
-						function onError(err){
-								triesLeft = tries - 1;
-								if(!triesLeft){
-										throw err;
-								}
-								return sleep(delay).then(() => fetchRetry(url, delay, triesLeft));
-						}
-						return fetch(url).then((res) => {
-						if (res.ok) {
-								return url
-							}
-							throw new Error("HTTP status " + res.status)
-						})
-						.catch(onError);
-				}
-			</script>
-			<p>The names are going to be:</p>
-			<ul>
-				{{range.}}
-					<li id="{{.}}"><i class="fa fa-spinner fa-spin" style="font-size:24px"></i></li>
-				{{end}}
-			</ul>
-			<script type="text/javascript">
-				async function getImg() {
-					await sleep(10000)
-					{{range.}}
-						console.log("loaded script for {{.}}")
-						console.log("fetching {{.}}")
-						await fetchRetry("http://localhost:3000/img/out_{{.}}.jpg", 2000, 10).then((val) => {
-							el = document.getElementById("{{.}}")
-							var img = document.createElement('img')
-							img.src = "http://localhost:3000/img/out_{{.}}.jpg"
-							img.style.width = "20%;"
-							img.id = "{{.}}"
-							el.parentNode.replaceChild(img, el)
-						})
-					{{end}}
-				}
-				getImg()
-			</script>
-			</body></html>`
+			html := buildHTML()
 			tpl := template.Must(template.New("").Parse(html))
 			var fingerprints []string
 			for _, opt := range opts {
@@ -340,4 +291,58 @@ func writeToS3(file *os.File) {
 	}
 
 	fmt.Println("Uploaded", item)
+}
+
+func buildHTML() string {
+	html := `<html><body>
+			<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+			<script type="text/javascript">
+				async function sleep(ms) {
+					return new Promise(resolve => setTimeout(resolve, ms));
+				}
+			</script>
+			<script>
+				function fetchRetry(url, delay, tries) {
+						function onError(err){
+								triesLeft = tries - 1;
+								if(!triesLeft){
+										throw err;
+								}
+								return sleep(delay).then(() => fetchRetry(url, delay, triesLeft));
+						}
+						return fetch(url).then((res) => {
+						if (res.ok) {
+								return url
+							}
+							throw new Error("HTTP status " + res.status)
+						})
+						.catch(onError);
+				}
+			</script>
+			<p>The names are going to be:</p>
+			<ul>
+				{{range.}}
+					<li id="{{.}}"><i class="fa fa-spinner fa-spin" style="font-size:24px"></i></li>
+				{{end}}
+			</ul>
+			<script type="text/javascript">
+				async function getImg() {
+					await sleep(10000)
+					{{range.}}
+						console.log("loaded script for {{.}}")
+						console.log("fetching {{.}}")
+						await fetchRetry("http://localhost:3000/img/out_{{.}}.jpg", 2000, 10).then((val) => {
+							el = document.getElementById("{{.}}")
+							var img = document.createElement('img')
+							img.src = "http://localhost:3000/img/out_{{.}}.jpg"
+							img.style.width = "20%;"
+							img.id = "{{.}}"
+							el.parentNode.replaceChild(img, el)
+						})
+					{{end}}
+				}
+				getImg()
+			</script>
+			</body></html>`
+	return html
 }
